e7a6d89cce611ffc463c0d6008ad91abf34ccf1c
Jason Best <jbest@brit.org>
2017-08-15 16:24:44 -0500

    adding md5 and UUID

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -1,5 +1,19 @@
 import argparse
-import glob
+from hashlib import md5
+import uuid
+#import glob
+
+
+def md5hash(fname):
+	# from https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+	# using this approach to ensure larger files can be read into memory
+    #hash_md5 = hashlib.md5()
+    hash_md5 = md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
 
 # set up argument parser
 ap = argparse.ArgumentParser()
@@ -14,10 +28,18 @@ for imagePath in glob.glob(args["source"] + "/*.JPG"):
     # get filename and use as the model name
     #k = imagePath[imagePath.rfind("/") + 1:]
     print (imagePath)
+    #generate MD5 hash
+    print (md5hash(imagePath))
+    #generate UUID for JPG image
+    print (uuid.uuid4())
+    # check exist of archive file
+    # print archive filepath
+    # generate md5 of file
+    # generate UUID
 
 
-#generate MD5
-#generate UUID
+
+
 #read barcode(s)
 #if no barcode, compare to folder models
 #log JPG data
