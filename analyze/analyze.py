bbfc4d44f1018f6613318ed428b761d158d905f2
Jason Best <jbest@brit.org>
2017-08-17 17:15:59 -0500

    finally got file creation time working

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -2,8 +2,11 @@ import argparse
 from hashlib import md5
 import uuid
 import glob
-from datetime import datetime#, date, time
+from datetime import datetime, date
+import time
+#import date
 import os
+import platform
 from pyzbar.pyzbar import decode
 from PIL import Image
 
@@ -24,6 +27,24 @@ def md5hash(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+def creation_date(path_to_file):
+    # From https://stackoverflow.com/a/39501288
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
+
 # set up argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, \
@@ -33,23 +54,28 @@ ap.add_argument("-m", "--models", required=False, \
 args = vars(ap.parse_args())
 
 #TODO record time start of analysis
-#iterate JPG file list passed from args
+#iterate JPG files in directory passed from args
 directory_path = os.path.realpath(args["source"])
 print('Scanning directory:', directory_path)
-for imagePath in glob.glob(directory_path + "/*.JPG"):
+for image_path in glob.glob(directory_path + "/*.JPG"):
     scan_start_time = datetime.now()
-    print(scan_start_time)
-    # get filename and use as the model name
-    #k = imagePath[imagePath.rfind("/") + 1:]
-    print(imagePath)
+    #print(scan_start_time)
+    # Gather file data
+    print(image_path)
+    basename = os.path.basename(image_path)
+    file_name, file_extension = os.path.splitext(basename)
+    # Get file creation time
+    file_creation_time = datetime.fromtimestamp(creation_date(image_path))
+    print ('Time:', file_creation_time)
+
     #generate MD5 hash
-    print(md5hash(imagePath))
+    #print(md5hash(image_path))
     #generate UUID for JPG image
-    print(uuid.uuid4())
+    #print(uuid.uuid4())
     # check exist of archive file
     # print archive filepath
     # read barcodes from JPG
-    barcodes = decode(Image.open(imagePath))
+    barcodes = decode(Image.open(image_path))
     if barcodes:
         for barcode in barcodes:
             if str(barcode.type) in ACCEPTED_SYMBOLOGIES:
@@ -67,7 +93,7 @@ for imagePath in glob.glob(directory_path + "/*.JPG"):
         # if no models provided, or no match, set new filename to a default
     # TODO record scan finish time
     scan_end_time = datetime.now()
-    print(scan_end_time)
+    #print(scan_end_time)
     # TODO report analysis progress and ETA
     #log JPG data
     #log CR2 data
