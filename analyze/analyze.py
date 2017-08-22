6d75794612b16ba0f38b49e926003c507c325d7f
Jason Best <jbest@brit.org>
2017-08-22 14:42:59 -0500

    adding csv logging

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -2,20 +2,24 @@ import argparse
 from hashlib import md5
 import uuid
 import glob
-from datetime import datetime, date
-import time
-#import date
+from datetime import datetime #, date
+import re
+import csv
+#import time
 import os
 import platform
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+
 # File extensions that are scanned and logged
 INPUT_FILE_TYPES = ('.jpg', '.jpeg', '.JPG', '.JPEG')
 # File type extensions that are logged when filename matches a scanned input file
 ARCHIVE_FILE_TYPES = ('.CR2', '.cr2', '.RAW', '.raw')
+# Barcode symbologies accepted, others ignored
 ACCEPTED_SYMBOLOGIES = ['CODE39']
 #TODO add accepted barcode string patterns
+FIELD_DELIMITER = ',' # delimiter used in output CSV
 
 def md5hash(fname):
     # from https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
@@ -45,6 +49,33 @@ def creation_date(path_to_file):
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
+# Attempts to get actual path of files with correct case
+def get_actual_filename(name):
+    # From https://stackoverflow.com/a/14742779
+    dirs = name.split('\\')
+    # disk letter
+    test_name = [dirs[0].upper()]
+    for d in dirs[1:]:
+        test_name += ["%s[%s]" % (d[:-1], d[-1])]
+    res = glob.glob('\\'.join(test_name))
+    if not res:
+        #File not found
+        return None
+    return res[0]
+
+def get_actual_filename2(name):
+    # from https://stackoverflow.com/a/7133137
+    name = "%s[%s]" % (name[:-1], name[-1])
+    return glob.glob(name)#[0]
+
+def casedpath(path):
+    # from https://stackoverflow.com/a/35229734
+    r = glob.glob(re.sub(r'([^:/\\])(?=[/\\]|$)', r'[\1]', path))
+    return r and r[0] or path
+
+def log_file_data(path=None, file_name=None, file_hash=None, barcodes=None):
+    print(path, file_name, file_hash, file_uuid, barcodes)
+
 # set up argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, \
@@ -53,7 +84,21 @@ ap.add_argument("-m", "--models", required=False, \
     help="Path to the model file for folder identification through histogram analysis.")
 args = vars(ap.parse_args())
 
-#TODO record time start of analysis
+analysis_start_time = datetime.now()
+batch_id = uuid.uuid4()
+
+# Create file for results
+log_file_name = analysis_start_time.date().isoformat() + '_' + str(batch_id) + '.csv'# Convert date to string in ISO format 
+#log_file_name = log_file_name.replace(':', '--')  # replace : in startTime to make filename work in various file systems   
+print(log_file_name)
+reportFile = open(log_file_name, "w")
+reportWriter = csv.writer(reportFile, delimiter = FIELD_DELIMITER, escapechar='#')
+# write header
+reportWriter.writerow(["ImagePath", "DirPath" , "BaseName", "FileName", "FileExtension", "Code", "CodeType" , "Scan time"])
+#TODO extract name of imager from directory path and save in log file
+
+
+#TODO extract information from directory name (imager, station, etc)
 #iterate JPG files in directory passed from args
 directory_path = os.path.realpath(args["source"])
 print('Scanning directory:', directory_path)
@@ -67,7 +112,7 @@ for image_path in glob.glob(directory_path + "/*.JPG"):
     print(file_name, file_extension)
     # Get file creation time
     file_creation_time = datetime.fromtimestamp(creation_date(image_path))
-    print ('Time:', file_creation_time)
+    print('Time:', file_creation_time)
     #generate MD5 hash
     file_hash = md5hash(image_path)
     #generate UUID for JPG image
@@ -79,12 +124,22 @@ for image_path in glob.glob(directory_path + "/*.JPG"):
         potential_arch_file_name = file_name + archive_extension
         potential_arch_file_path = os.path.join(directory_path, potential_arch_file_name)
         # test if archive file exists
+        # TODO change filename comparison be case-sensitive
+
         if os.path.exists(potential_arch_file_path):
+            # Trying to get the path with correct case, file extension in particular
+            """
+            print('Matched ext:', archive_extension, 'Matched filename:', potential_arch_file_name)
+            print(os.path.realpath(potential_arch_file_path))
+            print(os.path.abspath(potential_arch_file_path))
+            print(casedpath(potential_arch_file_path))
+            arch_file_path = get_actual_filename2(potential_arch_file_path)
+            """
             arch_file_path = potential_arch_file_path
             break
 
     # print archive filepath
-    print ('Archive file:', arch_file_path)
+    print('Archive file:', arch_file_path)
     # read barcodes from JPG
     barcodes = decode(Image.open(image_path))
     if barcodes:
@@ -110,4 +165,8 @@ for image_path in glob.glob(directory_path + "/*.JPG"):
     #log CR2 data
     #write to DB?
 
-# TODO report total analysis time and stop time
+analysis_end_time = datetime.now()
+print('Started:', analysis_start_time)
+print('Completed:', analysis_end_time)
+print('Duration:', analysis_end_time - analysis_start_time)
+
