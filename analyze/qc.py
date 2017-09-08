1d01fecb24c93bbf9bb74f15fbb08cf50f365a07
Jason Best <jbest@brit.org>
2017-09-08 15:43:57 -0500

    new stub for quality control process

---
+++ analyze/qc.py
@@ -0,0 +1,9 @@
+"""
+find records with 'ambiguous' image classifications
+find records with multiple barcodes
+find records with high (bad match) closest models
+for each:
+display image, prompt to fix
+
+
+"""
