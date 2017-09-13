303cf11b7c7ed67481df3b268a3bbaf664b2cb57
Jason Best <jbest@brit.org>
2017-09-13 15:34:34 -0500

    fixed batch_flags bug

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -159,6 +159,9 @@ if args["models"]:
 if args["batch"]:
     batch_flags = args["batch"]
     print('Batch flags:', batch_flags)
+else:
+    batch_flags=None
+
 # Create file for results
 log_file_name = analysis_start_time.date().isoformat() + '_' + batch_id + '.csv'
 # Test output path
