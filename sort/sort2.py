3d23277d7d908d1bf8dde32d240140b7783bafac
Jason Best <jbest@brit.org>
2020-05-15 12:25:31 -0500

    added logging

--- sort/sort2.py
+++ sort/sort2.py
@@ -6,6 +6,8 @@ import argparse
 #import os
 import shutil
 from pathlib import Path
+import re
+import csv
 
 DEFAULT_HERBARIUM_PREFIX = 'BRIT'
 DEFAULT_FOLDER_INCREMENT = 1000
@@ -18,6 +20,8 @@ verbose = False
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, \
     help="Path to the directory that contains the images to be sorted.")
+ap.add_argument("-a", "--action_log", required=True, \
+    help="Path of file where action log is stored.")
 ap.add_argument("-o", "--output_directory", required=True, default=None,\
     help="Path to an existing directory where sorted files and directories will be written.")
 ap.add_argument("-p", "--pattern", required=True, \
@@ -42,9 +46,11 @@ PAD = int(args["length"])
 recurse_subdirectories = args["recursive"]
 dry_run = args["dry_run"]
 
+
 def sort_file(source=None, destination=None):
     global files_sorted
     if destination.exists():
+        logwriter.writerow(['test_time', source, destination, 'ERROR: file exists'])
         if dry_run:
             print('DRY-RUN: Filename exists, cannot move:', destination)
         if verbose:
@@ -53,8 +59,11 @@ def sort_file(source=None, destination=None):
     else:
         if dry_run:
             print('DRY-RUN: Moved:', destination)
+            logwriter.writerow(['test_time', source, destination, 'DRY-RUN: moved'])
         else:
-            shutil.move(source, destination)
+            # temporary disable move
+            #shutil.move(source, destination)
+            logwriter.writerow(['test_time', source, destination, 'moved'])
             if verbose:
                 print('Moved:', destination)
         files_sorted += 1
@@ -89,45 +98,59 @@ else:
     path_matches = source_directory_path.glob(pattern)
 
 #for matching_path in source_directory_path.rglob('*.jpg'):
-for matching_path in path_matches:
-    files_analyzed += 1
-    #basename = os.path.basename(source_path)
-    basename = matching_path.name
-    if basename.startswith(HERBARIUM_PREFIX):
-        file_name = matching_path.stem
-        file_extension = matching_path.suffix
-        #print('file_name:', file_name)
-        #print('file_extension:', file_extension)
-        accession_id = file_name[len(HERBARIUM_PREFIX):]
-        try:
-            accession_number = int(accession_id)
-            folder_number = int(accession_number//FOLDER_INCREMENT*FOLDER_INCREMENT)
-            padded_folder_number = str(folder_number).zfill(PAD)
-            # zfill may be deprecated in future? Look into string formatting with fill
-            # https://stackoverflow.com/a/339013
-            #destination_folder_name = HERBARIUM_PREFIX + str(int(accession_number//FOLDER_INCREMENT*FOLDER_INCREMENT))
-            destination_folder_name = HERBARIUM_PREFIX + padded_folder_number
-            if output_directory:
-                output_directory_path = Path(output_directory)
-                destination_directory_path = output_directory_path.joinpath(destination_folder_name)
-            else:
-                # no output_directory specified, using source directory
-                destination_directory_path = source_directory_path.joinpath(destination_folder_name)
-            destination_file_path = destination_directory_path.joinpath(basename)
-            # Check if destination directory exists
-            if destination_directory_path.is_dir():
-                sort_file(source=matching_path, destination=destination_file_path)
-            else:
-                if verbose:
-                    print('Creating folder: ' + destination_directory_path)
-                destination_directory_path.mkdir()
-                sort_file(source=matching_path, destination=destination_file_path)
+# pattern for derivative files
+pattern_string = HERBARIUM_PREFIX + '(\d*)'
+accession_id_pattern = re.compile(pattern_string)
+# Create log file
+# TODO Make sure log file can be written and is unique
+# TODO Use filename specified in args
+with open('log_out.csv', 'w', newline='') as csvfile:
+    logwriter = csv.writer(csvfile)
+    # write header
+    logwriter.writerow(['timestamp', 'source', 'destination', 'action'])
+    #logwriter.writerow(['test_time', 'test_source', 'test_destination', 'test_action'])
+    for matching_path in path_matches:
+        files_analyzed += 1
+        #basename = os.path.basename(source_path)
+        basename = matching_path.name
+        if basename.startswith(HERBARIUM_PREFIX):
+            file_name = matching_path.stem
+            file_extension = matching_path.suffix
+            #print('file_name:', file_name)
+            #print('file_extension:', file_extension)
+            #accession_id = file_name[len(HERBARIUM_PREFIX):]
+            accession_match = accession_id_pattern.match(file_name)
+            try:
+                #accession_number = int(accession_id)
+                accession_number = int(accession_match.group(1))
+                folder_number = int(accession_number//FOLDER_INCREMENT*FOLDER_INCREMENT)
+                padded_folder_number = str(folder_number).zfill(PAD)
+                # zfill may be deprecated in future? Look into string formatting with fill
+                # https://stackoverflow.com/a/339013
+                #destination_folder_name = HERBARIUM_PREFIX + str(int(accession_number//FOLDER_INCREMENT*FOLDER_INCREMENT))
+                destination_folder_name = HERBARIUM_PREFIX + padded_folder_number
+                if output_directory:
+                    output_directory_path = Path(output_directory)
+                    destination_directory_path = output_directory_path.joinpath(destination_folder_name)
+                else:
+                    # no output_directory specified, using source directory
+                    destination_directory_path = source_directory_path.joinpath(destination_folder_name)
+                destination_file_path = destination_directory_path.joinpath(basename)
+                # Check if destination directory exists
+                if destination_directory_path.is_dir():
+                    sort_file(source=matching_path, destination=destination_file_path)
+                else:
+                    if verbose:
+                        print('Creating folder: ' + destination_directory_path)
+                    destination_directory_path.mkdir()
+                    sort_file(source=matching_path, destination=destination_file_path)
 
-        except ValueError:
-            print('Cannot parse', file_name)
-    else:
-        if verbose:
-            print(f'Ignoring {basename} - does not start with {HERBARIUM_PREFIX}.')
+            except ValueError:
+                print('Cannot parse', file_name)
+        else:
+            if verbose:
+                print(f'Ignoring {basename} - does not start with {HERBARIUM_PREFIX}.')
+
 print('Sort complete.')
 print('Encountered files:', files_analyzed)
 print('Sorted files:', files_sorted)
