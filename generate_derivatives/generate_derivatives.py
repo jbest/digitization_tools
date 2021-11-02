4e9caaac7c08aacbd6e06f98d9c3364e1212c7de
Jason Best <jbest@brit.org>
2021-11-01 19:57:29 -0500

    improved progress bar, cleanup

--- generate_derivatives/generate_derivatives.py
+++ generate_derivatives/generate_derivatives.py
@@ -9,6 +9,7 @@ from PIL import Image
 import re
 import os
 from pathlib import Path
+from progress.bar import Bar
 
 
 THUMB_EXT = '_thumb'
@@ -18,7 +19,7 @@ MED_EXT = '_med'
 def arg_setup():
     # set up argument parser
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input_path", required=False, \
+    ap.add_argument("-i", "--input_path", required=True, \
         help="Input directory path - overrides input_path in config file")
     ap.add_argument("-v", "--verbose", action="store_true", \
         help="Detailed output.")
@@ -30,7 +31,6 @@ def arg_setup():
     return args
 
 def generate_derivatives(source_file=None):
-   print('Generating derivatives: ', str(source_file.name))
    image_directory = source_file.parent
    file_stem = source_file.stem
    thumb_path = image_directory.joinpath(file_stem + THUMB_EXT + source_file.suffix)
@@ -56,18 +56,17 @@ def scan_files(path=None):
     Scan the directory for files matching JPEG files.
     Determine file type based on ending qualifier (thumb, med, etc)
     """
-    #matches = []
-    #image_sets = []
+    matches = []
     pattern = '([^\\s]+(\\.(?i)(jpe?g))$)'
     scan_path = Path(path)
-    #print('pattern:', pattern)
     file_pattern = re.compile(pattern)
     for root, dirs, files in os.walk(path):
+        root_path = Path(root)
         for file in files:
-            #print(os.path.join(root, file))
+            # print(os.path.join(root, file))
             m = file_pattern.match(file)
             if m:
-                file_path = scan_path.joinpath(file)
+                file_path = root_path.joinpath(file)
                 file_stem = file_path.stem
                 # Evaluate file stem
                 if file_stem.endswith(THUMB_EXT):
@@ -76,8 +75,9 @@ def scan_files(path=None):
                     file_type = 'medium'
                 else:
                     file_type = 'full'
-                    #print('generate derivatives')
-                    generate_derivatives(source_file=file_path)
+                    # add to matches
+                    matches.append(file_path)
+    return matches
 
 if __name__ == '__main__':
     # set up argparse
@@ -86,9 +86,11 @@ if __name__ == '__main__':
     verbose = args['verbose']
     force_overwrite = args['force']
     input_path = args['input_path']
-    scan_files(path=input_path)
-
-
-
+    file_matches = scan_files(path=input_path)
+    bar = Bar('Processing', max=len(file_matches))
+    for file in file_matches:
+        generate_derivatives(source_file=file)
+        bar.next()
+    bar.finish()
 
 
